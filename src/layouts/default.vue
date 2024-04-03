df4ec373913dee3bb5882dd901e0e41c733ffdac
Ilya <ilya.0023.0@gmail.com>
2024-04-03 16:07:12 +0500

    chore(layouts): new layout - default

---
+++ src/layouts/default.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <WidgetsHeader />
+    <div class="bg">
+      <SVGBackground />
+    </div>
+    <div class="content">
+      <div class="cw">
+        <slot></slot>
+      </div>
+    </div>
+  </div>
+</template>
+<style lang="scss" scoped>
+.bg {
+  position: absolute;
+  top: $header-height;
+  left: 0;
+  overflow: hidden;
+  max-width: 100%;
+  /* z-index: -1; */
+}
+.content {
+  position: relative;
+  z-index: 1;
+  width: 100%;
+  padding: 1rem 1rem;
+  user-select: none;
+
+  .cw {
+    max-width: 90.625rem;
+    height: 100%;
+    margin: 0 auto;
+    gap: 1rem;
+  }
+}
+</style>
