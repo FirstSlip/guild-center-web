90434570abf270283b1a31c6481e2852331d8702
Ilya <ilya.0023.0@gmail.com>
2024-04-03 23:55:29 +0500

    chore(footer): new components - Footer - Contacts

---
+++ src/components/Widgets/Footer/Contacts/index.vue
@@ -0,0 +1,77 @@
+<template>
+  <p class="h3">Контакты</p>
+  <div class="contacts">
+    <div class="email">
+      <p class="title h4">Email:</p>
+      <UILink to="mailto:9cTqW@example.com">
+        9cTqW@example.com
+      </UILink>
+    </div>
+    <div class="socials">
+      <p class="title h4">Социальные сети</p>
+      <div class="icons">
+        <UILink
+          v-for="(contact, index) in contacts"
+          :to="contact.to"
+          padding=".75rem"
+        >
+          <component :is="contact.logo"></component>
+        </UILink>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import Vk from '@/components/SVG/Logo/Vk.vue';
+import YouTube from '@/components/SVG/Logo/YouTube.vue';
+import Discord from '@/components/SVG/Logo/Discord.vue';
+import Twitch from '@/components/SVG/Logo/Twitch.vue';
+const contacts = [
+  {
+    logo: Vk,
+    to: 'https://vk.com/'
+  },
+  {
+    logo: YouTube,
+    to: 'https://www.youtube.com/'
+  },
+  {
+    logo: Discord,
+    to: 'https://discord.com/'
+  },
+  {
+    logo: Twitch,
+    to: 'https://www.twitch.tv/'
+  }
+];
+</script>
+
+<style lang="scss" scoped>
+.contacts {
+  width: 100%;
+  margin: 2.125rem 0 2.5rem;
+  padding-bottom: 2rem;
+  display: flex;
+  gap: 3.25rem;
+
+  border-bottom: 2px solid #e2e2e2;
+
+  .email {
+    p.title {
+      margin-bottom: 1rem;
+    }
+
+    a {
+      user-select: text;
+      padding-right: 2rem;
+    }
+  }
+
+  .socials {
+    p.title {
+      margin-bottom: 1rem;
+    }
+  }
+}
+</style>
---
+++ src/components/Widgets/Footer/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <footer class="footer">
+    <div class="cw">
+      <!-- <WidgetsHeaderLogo />
+      <WidgetsHeaderMenu />
+      <WidgetsHeaderTools /> -->
+      <WidgetsFooterContacts />
+      <SVGLogo />
+    </div>
+  </footer>
+</template>
+<script>
+export default {};
+</script>
+<style lang="scss" scoped>
+footer.footer {
+  position: relative;
+  z-index: 1;
+  width: 100%;
+  padding: 1rem 1rem;
+  user-select: none;
+
+  background-color: $footer-background;
+  .cw {
+    max-width: $main-width;
+    height: 100%;
+    margin: 0 auto;
+    padding: 3.5rem 0 2.25rem;
+    gap: 1rem;
+  }
+}
+</style>
