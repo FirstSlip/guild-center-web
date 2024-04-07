3abb867566166c05cf0d5e3cbee5f4dfd72cb597
Ilya <ilya.0023.0@gmail.com>
2024-04-07 19:15:02 +0500

    chore: add animation styles for /auth pages

--- src/app.vue
+++ src/app.vue
@@ -1,7 +1,24 @@
 <template>
   <NuxtLayout>
-    <div>
-      <NuxtPage />
-    </div>
+    <NuxtPage />
   </NuxtLayout>
 </template>
+
+<style lang="scss">
+.page-auth-enter-active {
+  transition: all 0.4s;
+}
+.page-auth-leave-active {
+  transition: all 0.4s;
+}
+.page-auth-enter-from {
+  transform: translateX(100%);
+  opacity: 0;
+  filter: blur(1rem);
+}
+.page-auth-leave-to {
+  transform: translateX(-100%);
+  opacity: 0;
+  filter: blur(1rem);
+}
+</style>
