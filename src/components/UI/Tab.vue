270a4f855693c487ac0697231eeba097077402b7
Ilya <ilya.0023.0@gmail.com>
2024-04-13 21:26:16 +0500

    chore(ui): new components - Tab - TabGroup

---
+++ src/components/UI/Tab.vue
@@ -0,0 +1,35 @@
+<template>
+  <button
+    :class="['tab h5', active && 'active']"
+    @click="$emit('click')"
+  >
+    <slot></slot>
+  </button>
+</template>
+
+<script lang="ts" setup>
+defineProps<{
+  active?: boolean;
+}>();
+defineEmits(['click']);
+</script>
+
+<style lang="scss" scoped>
+button.tab {
+  border: none;
+  border-bottom: 2px solid;
+  border-bottom-color: transparent;
+  padding: 0rem 1.25rem;
+  color: $text-white;
+
+  background: none;
+
+  cursor: pointer;
+  transition: all 3s ease;
+
+  &.active {
+    border-color: $text-white;
+    border-bottom-color: $text-white;
+  }
+}
+</style>
---
+++ src/components/UI/TabGroup.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="tab-group">
+    <UITab
+      v-for="(tab, index) in tabs"
+      :key="index"
+      :active="activeTab === index"
+      @click="tabChange(index)"
+    >
+      {{ tab }}
+    </UITab>
+  </div>
+</template>
+
+<script lang="ts" setup>
+defineProps<{
+  tabs: string[];
+  activeTab: number;
+}>();
+const emits = defineEmits(['click']);
+
+const tabChange = (index: number) => {
+  emits('click', index);
+};
+</script>
+
+<style lang="scss" scoped>
+.tab-group {
+  display: flex;
+  gap: 1rem;
+}
+</style>
