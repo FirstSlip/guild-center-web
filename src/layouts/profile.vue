ce0e7c9d3a9f8e92c597beb368f7e14b142c6e1f
Ilya <ilya.0023.0@gmail.com>
2024-04-17 18:08:17 +0500

    chore(layouts): new layout - profile

---
+++ src/layouts/profile.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <WidgetsHeader />
+    <!-- <div class="bg">
+      <SVGBackground />
+    </div> -->
+    <div class="content">
+      <div class="cw">
+        <ProfileHeader />
+        <NuxtPage />
+      </div>
+    </div>
+    <WidgetsFooter />
+  </div>
+</template>
+<script setup lang="ts">
+const handleVnodeUpdated = () => {
+  console.log('vnode-updated');
+};
+</script>
+<style lang="scss" scoped>
+.bg {
+  position: absolute;
+  top: $header-height;
+  left: 0;
+  overflow: hidden;
+  max-width: 100%;
+  /* z-index: -1; */
+}
+.content {
+  width: 100%;
+  min-height: calc(100svh - $header-height);
+  padding: 1rem 1rem;
+
+  overflow: hidden;
+
+  .cw {
+    max-width: 53.12rem;
+    height: 100%;
+    margin: 0 auto;
+  }
+}
+</style>
