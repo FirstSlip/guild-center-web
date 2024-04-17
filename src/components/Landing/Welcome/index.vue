c8ce7a844306df92d6490305f83ad1cb4d7f4de1
Ilya <ilya.0023.0@gmail.com>
2024-04-17 19:37:55 +0500

    chore(Landing): new components - Landing - LandingWelcome

---
+++ src/components/Landing/Welcome/index.vue
@@ -0,0 +1,52 @@
+<template>
+  <section class="welcome">
+    <h1 class="h2">
+      Добро пожаловать на главную страницу
+      <br />
+      веб-сервиса "GuildCenter"
+    </h1>
+    <p class="p2">
+      Веб-сервис создан для нахождения, создания, управления
+      игровыми гильдиями
+    </p>
+    <div class="guild-buttons">
+      <UIButton
+        type="primary"
+        @click="useRouter().push('/auth/sign-up')"
+        transparent
+      >
+        Найти гильдию
+      </UIButton>
+      <UIButton
+        type="primary"
+        @click="useRouter().push('/auth/sign-up')"
+      >
+        Создать гильдию
+      </UIButton>
+    </div>
+  </section>
+</template>
+
+<script lang="ts" setup></script>
+
+<style lang="scss" scoped>
+section.welcome {
+  h1 {
+    text-align: center;
+  }
+
+  p {
+    margin-top: 1rem;
+
+    text-align: center;
+  }
+
+  .guild-buttons {
+    width: 100%;
+    margin-top: 2.5rem;
+    display: flex;
+    justify-content: center;
+    gap: 5.625rem;
+  }
+}
+</style>
---
+++ src/components/Landing/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="landing">
+    <LandingWelcome />
+  </div>
+</template>
+
+<script lang="ts" setup></script>
+
+<style lang="scss" scoped>
+.landing {
+  margin: 15% 0;
+}
+</style>
