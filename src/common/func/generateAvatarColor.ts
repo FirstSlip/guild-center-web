1c126594132fdbb31c16ececfd8048031526bca4
Ilya <ilya.0023.0@gmail.com>
2024-04-17 17:58:15 +0500

    chore(generateAvatarColors): change functionality to select from 8 colors

--- src/common/func/generateAvatarColor.ts
+++ src/common/func/generateAvatarColor.ts
@@ -1,33 +1,14 @@
 export const generateAvatarColor = (
-  username: string
+  username: string | undefined
 ): string => {
-  // Преобразуем строку в хэш-код
+  const totalColors = 8;
   let hash = 0;
-  for (let i = 0; i < username.length; i++) {
-    hash = username.charCodeAt(i) + ((hash << 5) - hash);
+  let name = username || 'Error';
+  for (let i = 0; i < name.length; i++) {
+    hash = name.charCodeAt(i) + ((hash << 5) - hash);
   }
 
-  const c = (hash & 0x00ffffff).toString(16).toUpperCase();
+  const index = Math.abs(hash % totalColors);
 
-  const r = parseInt(c.slice(0, 2), 16);
-  const g = parseInt(c.slice(2, 4), 16);
-  const b = parseInt(c.slice(4, 6), 16);
-
-  const brightness = (r * 299 + g * 587 + b * 114) / 1000;
-  const isTooLight = brightness > 200;
-  const isTooDark = brightness < 100;
-  const isGray =
-    Math.abs(r - g) < 10 &&
-    Math.abs(g - b) < 10 &&
-    Math.abs(b - r) < 10;
-  const isBright = (r + g + b) / 3 > 180;
-
-  if (isTooLight || isTooDark || isGray || isBright) {
-    const newR = Math.floor(Math.random() * 200);
-    const newG = Math.floor(Math.random() * 200);
-    const newB = Math.floor(Math.random() * 200);
-
-    return `#${newR.toString(16).padStart(2, '0')}${newG.toString(16).padStart(2, '0')}${newB.toString(16).padStart(2, '0')}`;
-  }
-  return `#${c.padStart(6, '0')}`;
+  return `var(--avatar-bg-${index + 1})`;
 };
