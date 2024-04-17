a432b486bb0446717342bb2988a85631de4e2eba
Ilya <ilya.0023.0@gmail.com>
2024-04-17 18:03:31 +0500

    refactor(Avatar): add avatar types - User - Guild

--- src/components/Widgets/Avatar/index.vue
+++ src/components/Widgets/Avatar/index.vue
@@ -1,36 +1,51 @@
 <template>
-  <img v-if="user.avatar" :src="user.avatar" :alt="user.name" />
+  <img
+    v-if="avatarUrl"
+    :src="avatarUrl"
+    :alt="name"
+    class="avatar img"
+  />
   <div
     v-else
-    class="user-avatar"
+    class="avatar icon"
     :style="`background-color: ${avatarColor}`"
   >
-    <SVGUser />
+    <SVGUser v-if="avatarType === 'User'" />
+    <SVGCastle v-else />
   </div>
 </template>
 
 <script lang="ts" setup>
 import { generateAvatarColor } from '@/common/func/generateAvatarColor';
-import type { User } from '@/ts/User';
 
 const props = defineProps<{
-  user: User;
+  type?: 'User' | 'Guild';
+  avatarUrl?: string;
+  name: string;
 }>();
 
+const avatarType = computed(() => props.type || 'User');
+
 const avatarColor = computed(() =>
-  generateAvatarColor(props.user.name)
+  generateAvatarColor(props.name)
 );
 </script>
 
 <style lang="scss" scoped>
-.user-avatar {
+.avatar {
   width: 100%;
   height: 100%;
 
   border-radius: 100%;
 
-  display: flex;
-  align-items: center;
-  justify-content: center;
+  overflow: hidden;
+
+  &.icon {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    padding: 10%;
+  }
 }
 </style>
