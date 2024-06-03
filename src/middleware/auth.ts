d30ca8b50182fd23c9b61e900762d7cfeee408b1
Ilya <ilya.0023.0@gmail.com>
2024-06-03 21:16:56 +0500

    fix: delete logs from auth middleware

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,12 +1,8 @@
 export default defineNuxtRouteMiddleware((to, from) => {
-  /* console.log(to, from); */
-  console.log(useProfile().user.value);
   if (useProfile().user.value) {
     return abortNavigation();
   }
-  // In a real app you would probably not redirect every route to `/`
-  // however it is important to check `to.path` before redirecting or you
-  // might get an infinite redirect loop
+
   if (to.path === '/') {
     return navigateTo('/');
   }
