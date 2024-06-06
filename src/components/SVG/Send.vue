45bd16e79709752065cf9dd1da1c97de552314fc
Ilya <ilya.0023.0@gmail.com>
2024-06-06 12:58:13 +0500

    chore: new SVG components

---
+++ src/components/SVG/Send.vue
@@ -0,0 +1,17 @@
+<template>
+  <svg
+    width="21"
+    height="20"
+    viewBox="0 0 21 20"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M19.3388 1.3335L9.61608 11.0562M19.3388 1.3335L13.1516 19.0112L9.61608 11.0562M19.3388 1.3335L1.66113 7.52068L9.61608 11.0562"
+      stroke="#66FCF1"
+      stroke-width="1.41421"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+  </svg>
+</template>
