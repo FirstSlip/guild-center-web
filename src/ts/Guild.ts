0183096222daf16cedcd2183201d0a00095fc173
Ilya <ilya.0023.0@gmail.com>
2024-06-10 01:35:06 +0500

    chore: add GuildMember array type to members in Guild type

--- src/ts/Guild.ts
+++ src/ts/Guild.ts
@@ -1,11 +1,12 @@
 import type { Game } from '@/ts/Game';
+import type { GuildMember } from '@/ts/GuildMember';
 
 export type Guild = {
   guildId: string;
   name: string;
   avatar: string;
   banner: string;
-  membersLength: number;
+  members: GuildMember[];
   games: Game[];
   trends: string[];
   description: string;
