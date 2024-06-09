3aee258d386f0fc5f37e3274dce594476a0a5825
Ilya <ilya.0023.0@gmail.com>
2024-06-10 01:38:55 +0500

    refactor: move guild components from layout to page

--- src/layouts/guild/index.vue
+++ src/layouts/guild/index.vue
@@ -6,11 +6,7 @@
     </div> -->
     <div class="content">
       <div class="cw">
-        <div class="main-section">
-          <GuildMainHeader />
-          <NuxtPage />
-        </div>
-        <GuildMainMembers />
+        <NuxtPage />
       </div>
     </div>
     <WidgetsFooter />
@@ -37,15 +33,6 @@
     max-width: $main-width;
     height: 100%;
     margin: 0 auto;
-
-    display: grid;
-    grid-template-columns: minmax(0, 1fr) 17.75rem;
-    gap: 1.25rem;
-
-    .main-section {
-      width: 100%;
-      height: 100%;
-    }
   }
 }
 </style>
--- src/pages/guild/[id]/index.vue
+++ src/pages/guild/[id]/index.vue
@@ -1,9 +1,47 @@
 <template>
-  <GuildMainDescription />
+  <div class="guild-page">
+    <div class="main-section">
+      <GuildMainHeader
+        :current-tab="selectedTab"
+        :guild="guild"
+        :guild-id="id"
+      />
+      <GuildMainDescription />
+    </div>
+    <GuildMainMembers :members="guild?.members || []" />
+  </div>
 </template>
 
 <script lang="ts" setup>
+const route = useRoute();
+const id = computed(() => route.params.id);
+const selectedTab = computed(() => route.query.tab || '');
+
+const { data: guild, refresh } = useAsyncData(
+  `user_${id.value}`,
+  async () => {
+    const response = await $api.guild.getById(id.value);
+    if ($api.utils.isSuccess(response)) {
+      return response.data.guild;
+    }
+    return null;
+  }
+);
+
+/* console.log('guild', guild.value); */
+
 definePageMeta({ layout: 'guild' });
 </script>
 
-<style></style>
+<style lang="scss" scoped>
+.guild-page {
+  display: grid;
+  grid-template-columns: minmax(0, 1fr) 17.75rem;
+  gap: 1.25rem;
+
+  .main-section {
+    width: 100%;
+    height: 100%;
+  }
+}
+</style>
