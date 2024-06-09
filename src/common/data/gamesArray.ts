233d6346f8ad0c74414f05ddb8c1e4d809041dc1
Ilya <ilya.0023.0@gmail.com>
2024-06-10 01:45:28 +0500

    chore: add constants - gamesArray - gameTypesArray - genresArray

---
+++ src/common/data/gamesArray.ts
@@ -0,0 +1,100 @@
+import type { Game } from '@/ts/Game';
+
+export const gamesArray: Game[] = [
+  {
+    name: 'World of Warcraft',
+    genres: ['MMORPG', 'MMO', 'RPG']
+  },
+  {
+    name: 'Final Fantasy XIV',
+    genres: ['MMORPG', 'MMO', 'RPG', 'JRPG']
+  },
+  {
+    name: 'Lost Ark',
+    genres: ['MMORPG', 'MMO', 'RPG']
+  },
+  {
+    name: 'Dota 2',
+    genres: ['MOBA']
+  },
+  {
+    name: 'League of Legends',
+    genres: ['MOBA']
+  },
+  {
+    name: 'Smite',
+    genres: ['MOBA']
+  },
+  {
+    name: 'Destiny 2',
+    genres: ['MMOFPS', 'MMO', 'FPS']
+  },
+  {
+    name: 'Warframe',
+    genres: ['Action RPG', 'TPS']
+  },
+  {
+    name: 'Counter-Strike 2',
+    genres: ['FPS']
+  },
+  {
+    name: 'Squad',
+    genres: ['FPS']
+  },
+  {
+    name: 'Escape from Tarkov',
+    genres: ['FPS', 'MMO', 'Выживание', 'RPG']
+  },
+  {
+    name: 'Helldivers 2',
+    genres: ['Action RPG', 'Co-Op']
+  },
+  {
+    name: 'Genshin Impact',
+    genres: ['Action RPG', 'Gacha']
+  },
+  {
+    name: 'Honkai: Star Rail',
+    genres: ['Пошаговая RPG', 'Gacha']
+  },
+  {
+    name: 'RAID: Shadow Legends',
+    genres: ['Collectible RPG', 'Пошаговая RPG', 'Gacha']
+  },
+  {
+    name: 'Roblox',
+    genres: [
+      'Симулятор',
+      'FPS',
+      'RPG',
+      'Гонки',
+      'Платформер',
+      'Хоррор',
+      'Головоломка'
+    ]
+  },
+  {
+    name: 'Minecraft',
+    genres: ['Песочница', 'Выживание', 'Творчество']
+  },
+  {
+    name: 'Rust',
+    genres: ['Песочница', 'Выживание', 'FPS', 'MMO']
+  },
+  {
+    name: 'Garry’s Mod',
+    genres: ['Песочница']
+  },
+  {
+    name: 'Dead by Daylight',
+    genres: ['Asymmetric multiplayer', 'Хоррор']
+  },
+  {
+    name: 'Phasmophobia',
+    genres: ['Co-Op', 'Хоррор']
+  },
+  {
+    name: 'GTFO',
+    genres: ['FPS', 'Хоррор']
+  }
+];
