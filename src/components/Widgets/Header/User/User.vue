5ea5433e419496550738522f32604ad3094d0d1f
Ilya <ilya.0023.0@gmail.com>
2024-06-10 02:19:26 +0500

    refactor(WidgetsHeaderUser): refactor component logic

--- src/components/Widgets/Header/User/User.vue
+++ src/components/Widgets/Header/User/User.vue
@@ -1,28 +1,29 @@
 <template>
   <div class="user">
     <div class="links">
-      <nuxt-link :to="`/profile_${user.tag}/friends`">
+      <nuxt-link :to="`/profile_${profile.tag}/friends`">
         <SVGFriends />
+        <div class="notification" v-if="haveRequests" />
       </nuxt-link>
       <nuxt-link to="/chat">
         <SVGChat />
       </nuxt-link>
-      <nuxt-link :to="`/profile_${user.tag}/guilds`">
+      <nuxt-link :to="`/profile_${profile.tag}/guilds`">
         <SVGCastle stroke="#66FCF1" />
       </nuxt-link>
     </div>
     <div class="profile">
-      <nuxt-link :to="`/profile_${user.tag}`" class="info">
+      <nuxt-link :to="`/profile_${profile.tag}`" class="info">
         <div class="user-avatar">
           <WidgetsAvatar
-            :name="user.username"
-            :avatarUrl="user.avatar"
+            :name="profile.username"
+            :avatarUrl="profile.avatar"
           />
         </div>
         <div class="username">
-          <span>{{ user.username }}</span>
+          <span>{{ profile.username }}</span>
           <br />
-          <span>#{{ user.tag }}</span>
+          <span>#{{ profile.tag }}</span>
         </div>
       </nuxt-link>
       <button
@@ -38,14 +39,20 @@
 </template>
 
 <script lang="ts" setup>
-import type { User } from '@/ts/User';
+import type { UserMe } from '@/ts/UserMe';
 
-defineProps<{
-  user: User;
+const props = defineProps<{
+  profile: UserMe;
 }>();
 defineEmits(['sign-out']);
 
 const signOutHover = ref(false);
+
+const haveRequests = computed(() => {
+  return props.profile.friendRequests.some(
+    (request) => request.recipient.tag === props.profile.tag
+  );
+});
 </script>
 
 <style lang="scss" scoped>
@@ -55,8 +62,8 @@ const signOutHover = ref(false);
   gap: 1.625rem;
   align-items: center;
 
-  @media (max-width: 1000px) {
-    flex-direction: column-reverse;
+  @media (max-width: $adaptive-breakpoint-normal) {
+    gap: 0.5rem;
   }
 
   .links {
@@ -65,7 +72,12 @@ const signOutHover = ref(false);
     align-items: center;
     gap: 1.5rem;
 
+    @media (max-width: $adaptive-breakpoint-normal) {
+      gap: 0.5rem;
+    }
+
     a {
+      position: relative;
       height: max(80%, 2rem);
       aspect-ratio: 1 / 1;
 
@@ -78,6 +90,22 @@ const signOutHover = ref(false);
         max-width: 2rem;
         max-height: 2rem;
       }
+
+      .notification {
+        position: absolute;
+        width: 1rem;
+        height: 1rem;
+        top: 0;
+        right: 0;
+        background-color: $red;
+        border-radius: 100%;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        font-size: 0.875rem;
+        font-weight: 700;
+        color: #fff;
+      }
     }
   }
 
@@ -85,6 +113,10 @@ const signOutHover = ref(false);
     display: flex;
     gap: 1.625rem;
     align-items: center;
+
+    @media (max-width: $adaptive-breakpoint-normal) {
+      gap: 0.25rem;
+    }
 
     .user-avatar {
       width: 3.125rem;
@@ -97,6 +129,12 @@ const signOutHover = ref(false);
       color: #fff;
 
       text-align: left;
+
+      min-width: 5.5rem;
+      max-width: 5.5rem;
+
+      overflow: hidden;
+      text-overflow: ellipsis;
 
       span {
         color: #fff;
